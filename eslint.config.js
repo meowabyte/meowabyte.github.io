e46c5d601d5f328f71e6853b833cc5b18b08660e
meowabyte <pojg77g1@kbb.anonaddy.me>
2025-06-05 19:56:55 +0200

    - update deps - add eslint (limited tailwind support) - add prettier

---
+++ eslint.config.js
@@ -0,0 +1,24 @@
+import js from "@eslint/js";
+import globals from "globals";
+import tseslint from "typescript-eslint";
+import pluginGitignore from "eslint-config-flat-gitignore";
+import css from "@eslint/css";
+import { defineConfig } from "eslint/config";
+import { tailwindSyntax } from "@eslint/css/syntax";
+
+export default defineConfig([
+    { files: ["**/*.{js,mjs,cjs,ts,mts,cts,jsx,tsx}"], plugins: { js }, extends: ["js/recommended"] },
+    { files: ["**/*.{js,mjs,cjs,ts,mts,cts,jsx,tsx}"], languageOptions: { globals: globals.browser } },
+    tseslint.configs.recommended,
+    pluginGitignore(),
+    {
+        files: ["**/*.css"],
+        plugins: { css },
+        language: "css/css",
+        extends: ["css/recommended"],
+        languageOptions: { customSyntax: tailwindSyntax },
+        // full tailwind support doesn't exist for theme() :(
+        // https://eslint.org/blog/2025/02/eslint-css-support/#custom-syntax-support
+        ignores: ["src/styles/globals.css"]
+    }
+]);
